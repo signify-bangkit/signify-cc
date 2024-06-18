3ba448490efc9c90d6b3f72a0c9aabc5bcbee3d8
Raihan <17radf@gmail.com>
2024-06-18 12:16:19 +0700

    disable token limit and convert response to json

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,7 +6,7 @@ const { createUser, getUserByEmail } = require('../models/userModels');
 exports.register = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+    return res.status(400).json({ error: true, msg: errors.errors[0].msg });
   }
 
   try {
@@ -14,22 +14,22 @@ exports.register = async (req, res) => {
 
     const userDoc = await getUserByEmail(email);
     if (userDoc.exists) {
-      return res.status(400).send('User already exists');
+      return res.status(400).send({ error: true, msg: 'User already exists' });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
     await createUser(email, hashedPassword, firstName, lastName);
 
-    res.status(201).send('User registered successfully');
+    res.status(201).json({ error: false, msg: 'User registered successfully' });
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(500).send({ error: true, msg: error.message });
   }
 };
 
 exports.login = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+    return res.status(400).json({ error: true, msg: errors.errors[0].msg });
   }
 
   try {
@@ -37,23 +37,28 @@ exports.login = async (req, res) => {
 
     const userDoc = await getUserByEmail(email);
     if (!userDoc.exists) {
-      return res.status(404).send('User not found');
+      return res.status(404).json({ error: true, msg: 'User not found' });
     }
 
     const user = userDoc.data();
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
-      return res.status(401).send('Invalid password');
+      return res.status(401).send({ error: true, msg: 'Invalid password' });
     }
 
     const token = jwt.sign(
       { email: user.email },
       process.env.JWT_SECRET_KEY,
-      {
-        expiresIn: '1h',
-      }
+      // {
+      //   expiresIn: '1h',
+      // }
     );
-    res.status(200).json({ token });
+    res.status(200).json({
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      token
+    });
   } catch (error) {
     res.status(500).send(error.message);
   }
