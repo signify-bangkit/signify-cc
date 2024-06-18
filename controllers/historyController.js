858d8adb587fe98bd0dd7784f0cd3c88d23ca778
Raihan <17radf@gmail.com>
2024-06-18 14:33:38 +0700

    add delete history feature

--- controllers/historyController.js
+++ controllers/historyController.js
@@ -9,9 +9,37 @@ exports.getHistory = async (req, res) => {
       .orderBy('createdAt', 'desc')
       .get();
 
-    const history = historySnapshot.docs.map((doc) => doc.data());
+    const history = historySnapshot.docs.map((doc) => {
+      return {
+        id: doc.id,
+        ...doc.data()
+      };
+    });
     console.log(historySnapshot.docs)
     res.status(200).json({ results: history });
+  } catch (error) {
+    res.status(500).json({ error: true, msg: error.message });
+  }
+};
+
+exports.deleteHistory = async (req, res) => {
+  try {
+    const { historyId } = req.body;
+
+    if (!historyId) {
+      return res.status(400).json({ error: true, msg: 'Missing required historyId in request body' });
+    }
+
+    const docRef = db.collection('history').doc(historyId);
+    const docSnapshot = await docRef.get();
+
+    if (!docSnapshot.exists) {
+      return res.status(404).json({ error: true, msg: 'History entry with the provided ID not found' });
+    }
+
+    await docRef.delete();
+
+    res.status(200).json({ error: true, message: 'History entry deleted successfully' });
   } catch (error) {
     res.status(500).json({ error: true, msg: error.message });
   }
--- routes/historyRoutes.js
+++ routes/historyRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { getHistory } = require('../controllers/historyController');
+const { getHistory, deleteHistory } = require('../controllers/historyController');
 const authenticate = require('../middleware/authenticate');  // Import authenticate middleware
 
 const router = express.Router();
 
 router.get('/', authenticate, getHistory);  // Pastikan rute ini benar
+router.delete('/delete', authenticate, deleteHistory);  // Pastikan rute ini benar
 
 module.exports = router;
